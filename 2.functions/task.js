fafdba63c1e32a3707e6ab40f4676f440a6df7aa
Irina <Feya_moon88@mail.ru>
2026-05-21 15:00:04 +0600

    парве задание по function выполнено

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,23 @@
 function getArrayParams(...arr) {
+  let min = Math.min(...arr);
+  let max = Math.max(...arr);
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  let avg = sum / arr.length;
+  let avgString = avg.toFixed(2);
+  let avgNumber = Number(avgString);
 
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
+function summElementsWorker(...arr) {}
 
-}
+function differenceMaxMinWorker(...arr) {}
 
-function differenceMaxMinWorker(...arr) {
+function differenceEvenOddWorker(...arr) {}
 
-}
+function averageEvenElementsWorker(...arr) {}
 
-function differenceEvenOddWorker(...arr) {
-
-}
-
-function averageEvenElementsWorker(...arr) {
-
-}
-
-function makeWork (arrOfArr, func) {
-
-}
+function makeWork(arrOfArr, func) {}
