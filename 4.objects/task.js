a4dc7e0586d19f85bf5a238dbc44f094e8b31e77
Irina <Feya_moon88@mail.ru>
2026-05-26 23:26:59 +0600

    выполнен первый пункт задачи

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,6 +1,11 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
+let student = new Student('Tanya', 'female', 17);
+let student2 = new Student('Alex', 'male', 18); 
 
 Student.prototype.setSubject = function (subjectName) {
   
