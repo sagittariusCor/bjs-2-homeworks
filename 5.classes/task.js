d8e0929d748ce7c0f6cbfa7b1451b55bb51c7586
Irina <Feya_moon88@mail.ru>
2026-06-03 09:22:51 +0600

    второе задание выполнено

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -61,3 +61,35 @@ class DetectiveBook extends Book {
         this.type = 'detective';
     }
 }  
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        for (let book of this.books) {
+            if (book[type] === value) {
+                return book;
+            }
+        }
+        return null;
+    }
+
+    giveBookByName(bookName) {
+        for (let i = 0; i < this.books.length; i++) {
+            if (this.books[i].name === bookName) {
+                return this.books.splice(i, 1)[0];
+            }
+        }
+        return null;
+    }
+}
+
