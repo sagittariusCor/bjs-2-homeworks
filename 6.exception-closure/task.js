7749ffde6b752205f6f3e16881f5ba122ab790a1
Irina <Feya_moon88@mail.ru>
2026-06-11 12:32:56 +0600

    второе задание пыполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -15,3 +15,41 @@ function validateCount(value) {
         return error;
     }
 }
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        }
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        const areaValue = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Number(areaValue.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        const triangle = new Triangle(a, b, c);
+        return triangle;
+    }
+    catch(error) {
+        return {
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area() {
+                return 'Ошибка! Треугольник не существует';
+            }
+        }
+    }
+}
