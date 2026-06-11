77e2c110430427936fb3050bae851cada9a6048d
Irina <Feya_moon88@mail.ru>
2026-06-11 11:25:07 +0600

    первое задание выполнено

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,17 @@
-﻿
+﻿function parseCount(value) {
+    const parsed = Number.parseFloat(value);
+    if (isNaN(parsed)) {
+        throw new Error('Невалидное значение');
+    }
+    return parsed;
+}
+
+function validateCount(value) {
+    try {
+        const result = parseCount(value);
+        return result;
+    }
+    catch(error) {
+        return error;
+    }
+}
